b92c8af3d6cf12e2e762b4ea512e3d33f8d7865f
ganeshbalaji111998 <ganeshbalajikingktr@gmail.com>
2022-09-20 10:38:12 +0530

    modified

--- UseBike.java
+++ UseBike.java
@@ -10,13 +10,12 @@ public class UseBike {
 		Bike b2 = new Bike("hero", 200000, "white", "Tn50 a2434");
 		Bike b3 = new Bike("yamaha", 150000, "red", "Tn50 d3454");
 		Bike b4 = new Bike("bajaj", 125000, "green", "Tn60 g8923");
-		Bike b5 = new Bike("bmw", 130000, "yellow", "Tn40 h8934");
 		HashMap<String, Bike> b = new HashMap<>();
 		b.put(b1.getNoPlate(), b1);
 		b.put(b2.getNoPlate(), b2);
 		b.put(b3.getNoPlate(), b3);
 		b.put(b4.getNoPlate(), b4);
-		b.put(b5.getNoPlate(), b5);
+	
 		for (String x : b.keySet()) {
 			System.out.println(b.get(x).getBrand());
 		}
@@ -26,7 +25,7 @@ public class UseBike {
 		int max=0;
 		for (Bike x : b.values()) {
 			
-			if(x.getPrice()>max) {
+			if(x.getPrice()<max) {
 				max=x.getPrice();
 			}	
 			}
